5e119c5e6f295a8bbfc83b818fa043b0381b0863
SawantAchal <achals1718@gmail.com>
2024-12-10 02:38:26 +0530

    import toast and mount all component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,28 @@
+
 import './App.css'
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import TaskFilter from './components/TaskFilter';
+import TaskTable from './components/TaskTable';
+import TaskFormModel from './components/TaskFormModel';
+import { useState } from 'react';
+import TaskCounter from './components/TaskCounter';
 
 function App() {
+  const [showModal, setShowModal] = useState(false);
 
   return (
     <>
-
+    <ToastContainer/>
+    <div className="p-4">
+      <TaskFilter />
+      <TaskCounter/>
+        <button className="bg-blue-500 text-white px-4 py-2 rounded" onClick={() => setShowModal(true)} >
+          Add Task
+        </button>
+        <TaskTable />
+        <TaskFormModel showModal={showModal} setShowModal={setShowModal} />
+      </div>
     </>
   )
 }
