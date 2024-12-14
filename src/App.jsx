b510131dc885489735b43a52e9bbb8dc99265524
SawantAchal <achals1718@gmail.com>
2024-12-15 01:33:55 +0530

    change css

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,33 @@
-
-import './App.css'
-import { ToastContainer, toast } from "react-toastify";
+import './App.css';
+import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import TaskFilter from './components/TaskFilter';
 import TaskTable from './components/TaskTable';
 import TaskFormModel from './components/TaskFormModel';
 import { useState } from 'react';
 import TaskCounter from './components/TaskCounter';
+import 'tabulator-tables/dist/css/tabulator.min.css';
 
 function App() {
   const [showModal, setShowModal] = useState(false);
 
   return (
     <>
-    <ToastContainer/>
-    <div className="p-4">
-      <TaskFilter />
-      <TaskCounter/>
-        <button className="bg-blue-500 text-white px-4 py-2 rounded" onClick={() => setShowModal(true)} >
-          Add Task
-        </button>
+      <ToastContainer />
+      <div className="p-4">
+        <div className="bg-gray-800 text-white p-4 rounded-md flex flex-col sm:flex-row justify-between items-center mb-4 shadow-md">
+          <TaskFilter />
+          <button className="bg-blue-500 text-white px-6 py-2 rounded-lg hover:bg-blue-600 transition duration-300 w-full sm:w-auto mt-4 sm:mt-0" onClick={() => setShowModal(true)}>
+            Add Task
+          </button>
+        </div>
+        <TaskCounter />
         <TaskTable />
         <TaskFormModel showModal={showModal} setShowModal={setShowModal} />
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
+
